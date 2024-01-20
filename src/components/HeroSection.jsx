843b288b6607aaa1cd5652ff1c4a50cffb02c7ae
Amir ali <amiralikhamseh01@gmail.com>
2024-01-20 21:09:03 +0100

    Font family locally  downloaded & configured

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function HeroSection() {
   return (
     <>
-      <div className="h-screen bg-purple-300  flex items-center justify-center flex-col md:px-[20rem] md:py-[10rem]  p-[5rem] ">
-        <h1 className=" text-4xl md:text-8xl  text-center font-bold tracking-tight text-gray-900 ">
+      <div className="h-screen bg-purple-300  flex items-center flex-col md:px-[20rem]   p-[5rem] ">
+        <h1 className=" text-4xl md:text-8xl  text-center font-bold tracking-tight text-gray-900 mt-[8rem]">
           Offset your CO2 emissions reliably, in Europe
         </h1>
         <h2 className="text-4xl md:text-6xl tracking-tight text-gray-900 text-center mt-2 ">
@@ -17,13 +17,13 @@ function HeroSection() {
         <div className="mt-10 flex items-center justify-center gap-x-6">
           <a
             href="https://www.fortomorrow.eu/en#our-approach"
-            className="focus:outline-none text-white bg-purple-950 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5"
+            className="focus:outline-none text-white bg-purple-950 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-xl px-5 py-2.5"
           >
             Learn more
           </a>
           <a
             href="https://www.fortomorrow.eu/en/co2-offsetting-companies"
-            className="flex  focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5  dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
+            className="flex items-center focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-xl px-5 py-2.5  dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
           >
             <span>For organizations</span>
             <svg
